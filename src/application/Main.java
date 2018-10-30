7b6f4e9b8ae4d741a37320cabed29ff5e0d9f362
a.gariepy <a.gariepy@400G4-442-22.interne.college-gerald-godin.qc.ca>
2018-10-30 16:50:28 -0400

    plpl

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,13 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,13 +16,17 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub		
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root, 400, 300);
-		
+		GridPane root = new GridPane();
+		Scene scene = new Scene(root, 500, 400);
+		BackgroundImage BGMain= new BackgroundImage(new Image("Background.png",32,32,false,true),
+		        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+		          BackgroundSize.DEFAULT);
+		Background bg1 = new Background(BGMain);
+		root.setBackground(bg1);
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		primaryStage.setTitle("Projet 2");
+		primaryStage.setTitle("Bienvenue");
 		primaryStage.setResizable(false);
 		primaryStage.sizeToScene();
 		
