54919a7647762fa7cb858c9ec20d362dd3994a0c
a.gariepy <a.gariepy@400G4-442-22.interne.college-gerald-godin.qc.ca>
2018-11-01 09:12:02 -0400

    second window

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,5 @@
 package application;
 
-import javax.swing.border.EtchedBorder;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,35 +10,54 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.RadialGradient;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class Main extends Application {
 
+public class Main extends Application {
+	private BorderPane root;
+	private Scene scene;
+	
+	
+	private EventHandler<ActionEvent> gestionUsager = new EventHandler<ActionEvent>() {
+
+		@Override
+		public void handle(ActionEvent event) {
+			// TODO Auto-generated method stub
+            
+            StackPane secondaryLayout = new StackPane();
+
+            Scene secondScene = new Scene(secondaryLayout, 800, 700);
+
+            // New window (Stage)
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Deuxième fenêtre");
+            newWindow.setScene(secondScene);
+
+            newWindow.show();
+			
+		}
+		
+};
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root, 500, 400);
+		root  = new BorderPane();
+		scene = new Scene(root, 500, 400);
 		Image image = new Image("Background.png");
 		BackgroundSize bgTaille = new BackgroundSize(500, 400, false, false, false, false);
 		BackgroundImage BGMain = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,BackgroundPosition.DEFAULT, bgTaille);
 		Background bg1 = new Background(BGMain);
-		Background bg2 = new Background(new BackgroundFill(Color.DIMGRAY, new CornerRadii(5), Insets.EMPTY));
 		root.setBackground(bg1);
 		root.setPadding(new Insets(10));
 		
@@ -57,6 +74,8 @@ public class Main extends Application {
 		Button btnUsager = new Button("Êtes vous un usager?");
 		Button btnConnexion = new Button("Connexion");
 		btnUsager.setOnAction(gestionUsager);
+		btnConnexion.setOnMouseClicked(e -> primaryStage.hide());
+		btnConnexion.setOnAction(gestionUsager);
 		
 		labelNoEmploye.setFont(Font.font("Arial", 15));
 		labelNoEmploye.setTextFill(Color.WHITE);
@@ -79,28 +98,10 @@ public class Main extends Application {
 		primaryStage.setTitle("Bienvenue à la médiathèque");
 		primaryStage.setResizable(false);
 		primaryStage.sizeToScene();
+		
 
 	}
-	private EventHandler<ActionEvent> gestionUsager = new EventHandler<ActionEvent>() {
-	public void handle(ActionEvent event) {
-		 
-        Label secondLabel = new Label("I'm a Label on new Window");
-
-        StackPane secondaryLayout = new StackPane();
-        secondaryLayout.getChildren().add(secondLabel);
-
-        Scene secondScene = new Scene(secondaryLayout, 230, 100);
-
-        // New window (Stage)
-        Stage newWindow = new Stage();
-        newWindow.setTitle("Second Stage");
-        newWindow.setScene(secondScene);
-
-        // Set position of second window, related to primary window.
-
-        newWindow.show();
-	}
-};
+	
 
 	public static void main(String[] args) {
 		launch(args);
